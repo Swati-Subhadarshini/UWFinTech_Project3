e30a9ed794c9887022a85ae57ebf54f6774575d4
joshuacreveling <josh.creveling22@gmail.com>
2022-01-25 21:16:26 -0800

    Working on Solidity contract.

--- josh_working_file/contracts/bet_slip.sol
+++ josh_working_file/contracts/bet_slip.sol
@@ -2,35 +2,59 @@ pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
-contract Certificate is ERC721Full {
+contract betWithFriends is ERC721Full {
+
+    address payable owner = msg.sender;
+    uint public accountBalance;
+    address payable authorizedRecipient;
+
     constructor() public ERC721Full("BetSlip", "BET") {}
 
     struct Bet {
         string username;
-        string matchup;
-        uint256 amountBet;
-        uint256 amountPayable;
+        string betSelection;
+        uint256 amountOfWager;
+        uint256 earnedPayout; 
     }
 
     mapping(uint256 => Bet) public betHistory;
 
-    event completeBet(uint256 betID, uint256 initalBet, uint256 amountPayable);
+    event completeBet(uint256 betID, uint256 earnedPayout);
 
-    function placeBet(
-        address user,
+    function placeBet (
+        address payable user,
         string memory username,
-        string memory matchup,
-        uint256 amountBet,
-        uint256 amountPayable
+        string memory betSelection
         )
-        public returns (uint256)
-    {
+        public payable {
+        uint256 amountOfWager = msg.value;
         uint256 betID = totalSupply();
-        amountPayable = 0;
+        uint256 earnedPayout = 0;
+        address payable receiver = address(this);
+
         _mint(user, betID);
 
-        betHistory[betID] = Bet(username, matchup, amountBet, amountPayable);
+        betHistory[betID] = Bet(username, betSelection, amountOfWager, earnedPayout);
 
-        return betID;
+        bool sent = receiver.send(msg.value);
+        require(sent, "Failed to send Ether");
+        accountBalance = address(this).balance;
     }
+
+    function updateBet(
+        uint256 betID,
+        uint256 newEarnedPayout
+    ) public returns (uint256) {
+        betHistory[betID].earnedPayout = newEarnedPayout;
+
+        emit completeBet(betID, newEarnedPayout);
+
+        return betHistory[betID].earnedPayout;
+    }
+
+    function deposit() public payable {
+        accountBalance = address(this).balance;
+    }
+
+    function() external payable {}
 }
