1600f16fddaf3446a34669bc7ca42c40527e5af0
Jacob_Vissering <jake.vissering@gmail.com>
2022-01-26 18:22:29 -0800

    Add Cash Bet Widget & Comments

--- jakes_work/Streamlit_Modules/oddsinterface.py
+++ jakes_work/Streamlit_Modules/oddsinterface.py
@@ -1,19 +1,30 @@
+# Library Imports
 import streamlit as st
 from pathlib import Path
 import pandas as pd
+import os
+from dotenv import load_dotenv
+from web3 import Web3
 
-#Jake's Work/Python_Modules/Resources/upcoming_games.csv
+
+# Python Code & Python Module Imports
+################################################################################
+
 # Import upcoming_games.csv
 upcoming_games = pd.read_csv(Path("../Python_Modules/Resources/upcoming_games.csv"))
 upcoming_games = upcoming_games.drop(columns="Unnamed: 0")
 
 # Create games in CSV as individual variables.
+# Could potentially create some sort of function for-loop that checks how many rows are in the upcoming_games.csv
+# and returns that many Team variables.
 Team_1 = f"{upcoming_games.iloc[0,2]} : {upcoming_games.iloc[0,3]}"
 Team_2 = f"{upcoming_games.iloc[0,4]} : {upcoming_games.iloc[0,5]}"
 Team_3 = f"{upcoming_games.iloc[1,2]} : {upcoming_games.iloc[1,3]}"
 Team_4 = f"{upcoming_games.iloc[1,4]} : {upcoming_games.iloc[1,5]}"
 
-# Here begins the Streamlit Interface code.
+
+# Streamlit Interface code.
+##################################################################################
 
 # Cover Image & Titles
 st.image("./Footballfield.jpeg")
@@ -30,7 +41,14 @@ with st.form(key='place_bet'):
     user_name = st.text_input('Enter your UserName')
     user_bet_selection = st.selectbox('Choose YOUR winner:', [Team_1, Team_2, Team_3, Team_4])
     user_wager = st.number_input('Wager Amount')
+    # Potential payout: We need to find a good way to take the odds from the bet selection and do the math to calculate the payout.
+    # Probably an if statement. 
+        # If odds are positive:
+        # odds / 100 * wager = Potential payout
+        # If odds are negative:
+        # 100 / odds * wager = Potential payout
     potential_payout = st.text('Potential Payout Placeholder')
+    # earned_payout will be 0 unless the bet wins and then it will equal potential_payout
     earned_payout = st.text('Earned Payout Placeholder')
     submitted = submit_button = st.form_submit_button(label='Submit Bet')
     if submitted:
@@ -45,10 +63,17 @@ with st.form(key='place_bet'):
         st.write("BetID")
 
 # Call block function. Checks to see if bet has finished.
-st.markdown('## Check Bet Status')
-with st.form(key="check_bet"):
+st.sidebar.markdown('## Check Bet Status')
+with st.sidebar.form(key="check_bet"):
     user_betID = st.text_input('Input your BetID')
     submitted = submit_button = st.form_submit_button(label='Check Bet Status')
     if submitted:
         st.write('Run check bet function')
 
+# Payout bet form & function
+st.sidebar.markdown('## Cash-In Winning Bet')
+with st.sidebar.form(key="cash_bet"):
+    user_betID = st.text_input('Input your BetID')
+    submitted = submit_button = st.form_submit_button(label='Cash Bet')
+    if submitted:
+        st.write('Run cash bet function')
