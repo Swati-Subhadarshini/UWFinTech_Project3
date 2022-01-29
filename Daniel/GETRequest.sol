9b889a352ae72eeb16e03d058038b2ac9adac9ce
Daniel McCaffrey <McCaffrey.danny@gmail.com>
2022-01-29 08:06:56 -0800

    Create GETRequest.sol

---
+++ Daniel/GETRequest.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
+ */
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
+ * PLEASE DO NOT USE THIS CODE IN PRODUCTION.
+ */
+contract APIConsumer is ChainlinkClient {
+    using Chainlink for Chainlink.Request;
+  
+    uint256 public volume;
+    
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+    
+    /**
+     * Network: Kovan
+     * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Devrel   
+     * Node)
+     * Job ID: d5270d1c311941d0b08bead21fea7747
+     * Fee: 0.1 LINK
+     */
+    constructor() {
+        setPublicChainlinkToken();
+        oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
+        jobId = "d5270d1c311941d0b08bead21fea7747";
+        fee = 0.1 * 10 ** 18; // (Varies by network and job)
+    }
+    
+    /**
+     * Create a Chainlink request to retrieve API response, find the target
+     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
+     */
+    function requestVolumeData() public returns (bytes32 requestId) 
+    {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        
+        // Set the URL to perform the GET request on
+        request.add("get", "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=75b5278235ced64e05fc1c7abd1a6014&regions=us&markets=h2h,spreads&oddsFormat=american");
+        
+        // Set the path to find the desired data in the API response, where the response format is:
+        //"id": "bda33adca828c09dc3cac3a856aef176",
+        //"sport_key": "americanfootball_nfl",
+        //"commence_time": "2021-09-10T00:20:00Z",
+        //"home_team": "Tampa Bay Buccaneers",
+        //"away_team": "Dallas Cowboys",
+        //"bookmakers": [
+        request.add("path", "price");
+        
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int timesAmount = 10**18;
+        request.addInt("times", timesAmount);
+        
+        // Sends the request
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+    
+    /**
+     * Receive the response in the form of uint256
+     */ 
+    function fulfill(bytes32 _requestId, uint256 _odds) public recordChainlinkFulfillment(_requestId)
+    {
+        odds = _odds;
+    }
+    function withdrawLink() external {}
+    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
+}
