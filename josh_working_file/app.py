df5fcf11aa989cbfd09385adfecaa0c8498b6fcf
joshuacreveling <josh.creveling22@gmail.com>
2022-01-30 14:04:37 -0800

    Added 'betStatus' field

--- josh_working_file/app.py
+++ josh_working_file/app.py
@@ -45,8 +45,8 @@ contract = load_contract()
 # BET INPUTS
 ################################################################################
 games_list = ["IMPORT", "GAMES", "HERE"]
+status_list = ["Pending", "Winner", "Complete"]
 accounts = w3.eth.accounts
-#account = accounts[0]
 
 st.markdown("### USER: Bet Parameters")
 
@@ -71,13 +71,13 @@ st.markdown("### USER: Display Bet")
 betID = st.number_input("Enter a Bet Token ID to display", value=0, step=1)
 if st.button("Display Bet"):
     
-    username, bet_selection, wager, earned_payout = contract.functions.reviewBet(betID).call()
+    username, bet_selection, wager, earned_payout, bet_status = contract.functions.reviewBet(betID).call()
     
     st.write(f"Username:{username}")
     st.write(f"Selected Bet:{bet_selection}")
     st.write(f"Wager:{wager} Wei")
-    #st.write(f"Potential Payout:{potential_payout*1000000000000000000} Ether") #Add this in final streamlit app
     st.write(f"Earned Payout:{earned_payout} Wei")
+    st.write(f"Bet Status:{bet_status}")
 
 st.markdown("### USER: Cashout")
 if st.button("winnerCashout"):
@@ -93,10 +93,12 @@ if st.button("winnerCashout"):
 st.markdown("### Owner: Update Earned Payout")
 # Updates earneed payout (Only the owner of the contract can run this function.)
 new_earned_payout = st.number_input("Calculate Payout", min_value=0)
+new_bet_status = st.selectbox("Bet Selection", options=status_list)
 if st.button("updateBet"):
     try:
-        contract.functions.updateBet(betID, new_earned_payout).transact({'from': user_account, 'gas': 1000000})
+        contract.functions.updateBet(betID, new_earned_payout, new_bet_status).transact({'from': user_account, 'gas': 1000000})
         earned_payout = new_earned_payout
+        bet_status = new_bet_status
     except:
         st.write("You do not have permission for this.")
 
--- josh_working_file/contracts/bet_slip.sol
+++ josh_working_file/contracts/bet_slip.sol
@@ -22,48 +22,52 @@ contract betWithFriends is ERC721Full {
         string username;
         string betSelection;
         uint256 amountOfWager;
-        uint256 earnedPayout; 
+        uint256 earnedPayout;
+        string betStatus; 
     }
 
     mapping(uint256 => Bet) public betHistory;
 
-    event completeBet(uint256 betID, uint256 earnedPayout);
+    event completeBet(uint256 betID, uint256 earnedPayout, string betStatus);
 
     function placeBet (address payable user, string memory username, string memory betSelection) public payable {
         address payable customerID = msg.sender;
         uint256 amountOfWager = msg.value;
         uint256 betID = totalSupply();
         uint256 earnedPayout = 0;
+        string memory betStatus = "Pending";
         address payable receiver = address(this);
 
         require(msg.value.mul(50) <= address(this).balance, "Your bet is too large.");
 
         _mint(user, betID);
 
-        betHistory[betID] = Bet(customerID, username, betSelection, amountOfWager, earnedPayout);
+        betHistory[betID] = Bet(customerID, username, betSelection, amountOfWager, earnedPayout, betStatus);
 
         bool sent = receiver.send(amountOfWager);
         require(sent, "Failed to send Ether");
         accountBalance = address(this).balance;
     }
 
-    function reviewBet(uint256 betID) public view returns (string memory, string memory, uint256, uint256) {
+    function reviewBet(uint256 betID) public view returns (string memory, string memory, uint256, uint256, string memory) {
         
         string memory _username = betHistory[betID].username;
         string memory _betSelection = betHistory[betID].betSelection;
         uint256 _amountOfWager = betHistory[betID].amountOfWager;
         uint256 _earnedPayout = betHistory[betID].earnedPayout;
+        string memory _betStatus = betHistory[betID].betStatus;
         
-        return (_username, _betSelection, _amountOfWager, _earnedPayout);
+        return (_username, _betSelection, _amountOfWager, _earnedPayout, _betStatus);
     }
 
-    function updateBet(uint256 betID, uint256 newEarnedPayout) public onlyOwner returns (uint256) {
+    function updateBet(uint256 betID, uint256 newEarnedPayout, string memory newBetStatus) public onlyOwner returns (uint256, string memory) {
 
         betHistory[betID].earnedPayout = newEarnedPayout;
+        betHistory[betID].betStatus = newBetStatus;
 
-        emit completeBet(betID, newEarnedPayout);
+        emit completeBet(betID, newEarnedPayout, newBetStatus);
 
-        return betHistory[betID].earnedPayout;
+        return (betHistory[betID].earnedPayout, betHistory[betID].betStatus);
     }
 
     function transferProfits(uint amount, address payable recipient) public onlyOwner {
@@ -81,7 +85,8 @@ contract betWithFriends is ERC721Full {
         accountBalance = address(this).balance;
 
         betHistory[betID].earnedPayout = 0;
-        emit completeBet(betID, 0);
+        betHistory[betID].betStatus = "Completed";
+        emit completeBet(betID, 0, "Completed");
     }
 
     function deposit() public payable {
